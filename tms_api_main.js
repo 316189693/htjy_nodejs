c1ea474545edcc63e52b6a569fd5b139d391ba1e
will zheng <will.zheng@unisco.com>
2020-08-10 14:17:41 +0800

    dd

--- tms_api_main.js
+++ tms_api_main.js
@@ -1,65 +1,52 @@
 let config = require("./config.json");
+
 let fs = require('fs');
 let _ = require("lodash");
 let moment = require('moment');
 let express = require('express');
+let uuid = require('node-uuid');
 let morgan = require('morgan');
 let rfs = require('rotating-file-stream');
+
 let TmsRouter = require('./lib/route/TmsRouter');
-let uuid = require('node-uuid');
 let mysqlUtil = require("./lib/util/MysqlUtil");
 let log_util = require('./lib/util/log_util');
 let auth_service = require('./lib/service/auth/auth_service');
-let logger = log_util(config.logger_file, config.logger_path, "api");
+
 const P44_API_NAME = 'Project44';
-let accessLogStream = rfs.createStream('access.log', {
-    interval: '1d', // rotate daily
-    path: config.logger_path
-});
-
-let mysql = new mysqlUtil({
-    host: config.db_host_tms,
-    port: config.db_port_tms,
-    user: config.db_user_tms,
-    password: config.db_password_tms,
-    database: config.db_database_tms
-}, logger);
-
 let options = {
-    key: fs.readFileSync("freightapp_key.pem"),
-    cert: fs.readFileSync("freightapp_cert.pem")
+    config: null,
+    mysql: null,
+    log: null,
+    _: _,
+    moment: moment,
 };
+let app = express();
 
-
-startServer();
-
-async function startServer(){
-    await initConfig(config);
-    let serverPort = config.tms_api_port;
-    let app = express();
-    morgan.token('id', function getId(req) {
-        return req.id
-    })
-// access log
-    app.use(morgan(':id [:date[clf]] :status :response-time ms ":method :res[content-length] :url HTTP/:http-version" :remote-addr - :remote-user ":referrer" ":user-agent"', { stream: accessLogStream }));
-    app.use(assignId);
-
-// auth
-    app.use(auth_service(mysql, logger));
-
-    let router = express.Router();
-    let tmsRouter = new TmsRouter(config, logger, mysql, _, moment);
-    app.use("/tmsapi", tmsRouter.router(router));
-    app.use(handleError(logger));
-    let server = require("https").createServer(options,app);
-    server.listen(serverPort);
-    logger.info("server started");
-    process.on('unhandledRejection', error => {
-        logger.error(error);
-  });
+async function beforeStartServer(){
+    Object.assign(options, {mysql: initMysql()}); // generate mysql instance
+    Object.assign(options, {log: initServerLog()}); // generate server log instance
+    await initP44Config();
+    Object.assign(options, {config: config});
+}
+function initMysql(){
+    let logger = log_util(config.logger_file, config.logger_path, "mysql");
+    let mysql = new mysqlUtil({
+        host: config.db_host_tms,
+        port: config.db_port_tms,
+        user: config.db_user_tms,
+        password: config.db_password_tms,
+        database: config.db_database_tms
+    }, logger);
+    return mysql;
 }
 
-async function initConfig(){
+function initServerLog(){
+    let logger = log_util(config.logger_file, config.logger_path, "api");
+    return logger;
+}
+
+async function initP44Config(){
     let p44HostInfo = await getP44HostInfo();
     if (p44HostInfo) {
         Object.assign(config, p44HostInfo);
@@ -67,11 +54,66 @@ async function initConfig(){
 }
 
 
+
+function startServer(){
+    let https_options = {
+        key: fs.readFileSync("freightapp_key.pem"),
+        cert: fs.readFileSync("freightapp_cert.pem")
+    };
+    let serverPort = options.config.tms_api_port;
+
+    app.use(assignId);
+    morgan.token('id', function getId(req) {
+        return req.id
+    })
+// access log
+    let accessLogStream = rfs.createStream('access.log', {
+        interval: '1d', // rotate daily
+        path: config.logger_path
+    });
+    app.use(morgan(':id [:date[clf]] :status :response-time ms ":method :res[content-length] :url HTTP/:http-version" :remote-addr - :remote-user ":referrer" ":user-agent"', { stream: accessLogStream }));
+// auth
+    app.use(auth_service(options.mysql));
+
+    let router = express.Router();
+    let tmsRouter = new TmsRouter(options);
+    app.use("/tmsapi", tmsRouter.router(router));
+    app.use(handleError(options.log));
+    let server = require("https").createServer(https_options,app);
+    server.listen(serverPort);
+}
+
+function assignId(req, res, next) {
+    req.id = uuid.v4();
+    next()
+}
+
+function handleError(logger) {
+    return function (err, req, res, next) {
+        logger.error(err.stack || err.message);
+        let rst =Object.assign({},{'reference_id':req.id},
+                                  {"error":err,
+                                   "stack": (err.response && err.response.data) || err.stack || err.message
+                                  }
+                              );
+        res.status(err.status || 500);
+        res.json(rst);
+    }
+}
+
+function afterStartServer(){
+    options.log.info("server started");
+    process.on('unhandledRejection', error => {
+        options.log.error(error);
+});
+}
+
+
 async function getP44HostInfo(){
     let sql = ` select api_manager.api_manager_url, api_manager_lines.api_manager_lines_key, api_manager_lines.api_manager_lines_value  from api_manager
                 inner join api_manager_lines on fk_api_manager_id = api_manager_id 
                 where api_manager_name ='${P44_API_NAME}' and api_manager_type='Outbound' limit 2; `;
-    let raws = await mysql.query(sql);
+    let raws = await options.mysql.query(sql);
     if (!raws || raws.length != 2) {
         return null;
     } else {
@@ -95,23 +137,13 @@ async function getP44HostInfo(){
     }
 }
 
+async function start(){
+    await beforeStartServer();
+    startServer();
+    afterStartServer();
+};
+start();
 
-function assignId(req, res, next) {
-    req.id = uuid.v4();
-    next()
-}
 
-function handleError(logger) {
-    return function (err, req, res, next) {
-        logger.error(err.stack || err.message);
-        let rst =Object.assign({},{'reference_id':req.id},
-                                  {"error":err,
-                                   "stack": (err.response && err.response.data) || err.stack || err.message
-                                  }
-                              );
-        res.status(err.status || 500);
-        res.json(rst);
-    }
-}
 
 
